3bc55d0002263879a248deef2186e4dfb4d222d8
MayaSelena773 <maya.bono721@gmail.com>
2022-07-27 19:00:49 +0200

    oop movie done

--- index.php
+++ index.php
@@ -5,12 +5,15 @@ class Movie {
     public $year;
     public $director;
 
-    public function__construct($_title, $_genre, $_year, $_director) {
+    public function __construct($_title, $_genre, $_year, $_director) {
         $this->title = $_title;
         $this->genre = $_genre;
         $this->year = $_year;
         $this->director = $_director;
+    }
 
+    public function originalLanguage() {
+        return 'english';
     }
 
 }
@@ -24,3 +27,25 @@ $harrypotter = new Movie('Harry Potter e la pietra filosofale', 'Fantasy, avvent
 $residentevil = new Movie('Resident Evil', 'Azione, fantascienza, horror', '2002', 'Paul W. S. Anderson');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php oop 1</title>
+</head>
+<body>
+
+<h1>Film in ordine di uscita:</h1>
+
+<ul>
+    <li><?php echo $harrypotter->title; ?> - <?php echo $harrypotter->year; ?></li>
+    <li><?php echo $signoredeglianelli->title; ?> - <?php echo $signoredeglianelli->year; ?></li>
+    <li><?php echo $residentevil->title; ?> - <?php echo $residentevil->year; ?></li>
+    <li><?php echo $killbill->title; ?> - <?php echo $killbill->year; ?></li>
+</ul>
+    
+</body>
+</html>
